8522f37bb4d4c68116992e5504fe79cf25b4c385
gregor <72101532+gregorb114@users.noreply.github.com>
2022-08-09 09:49:08 +0200

    Add files via upload

---
+++ pipe-remover.js
@@ -0,0 +1,17 @@
+//Issue
+//Looks like some hoodlum plumber and his brother has been running around and damaging your stages again.
+
+//The pipes connecting your level's stages together need to be fixed before you receive any more complaints.
+
+//Pipes list is correct when each pipe after the first index is greater (+1) than the previous one, and that there is no duplicates.
+
+//Task
+// Given the a list of numbers, return a fixed list so that the values increment by 1 for each index from the minimum value up to the maximum value (both included).
+
+function pipeFix(numbers){
+    var n_array = [];
+    for(var i=numbers[0];i<=numbers[numbers.length-1];i++){
+      n_array.push(i);
+    }
+    return n_array;
+  }
