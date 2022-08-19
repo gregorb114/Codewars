2eb5f693e4474757bb0361f819f092610dfb7113
gregor <gregor.boznar@gmail.com>
2022-08-19 22:29:28 +0200

    Add files via upload

---
+++ Double-char.js
@@ -0,0 +1,11 @@
+// Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+
+
+function doubleChar(str) {
+    let word = '';
+    for (let i = 0; i < str.length; i++){
+      word = word + str[i] + str[i];
+    };
+    return word;
+  };
+  
