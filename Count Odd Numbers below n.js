8967d489d9c2de69effe8dd4a5ab66edfc3ad6fa
gregor <gregor.boznar@gmail.com>
2022-09-03 23:44:14 +0200

    Add files via upload

---
+++ Count Odd Numbers below n.js
@@ -0,0 +1,9 @@
+//Given a number n, return the number of positive odd numbers below n, EASY!
+
+function oddCount(n){
+    // your code here
+    console.log(n)
+    let result = 0
+    result = Math.floor(n/2)
+    return result
+  }
