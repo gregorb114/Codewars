b1336c79e2392d6dc550d0d61611a1bab549afb6
gregor <gregor.boznar@gmail.com>
2022-09-23 23:39:45 +0200

    Add files via upload

---
+++ speed.js
@@ -0,0 +1,9 @@
+// The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+// For example:
+
+// 1.08 --> 30
+
+function cockroachSpeed(s) {
+    return Math.floor(s*100000/3600);
+   }
