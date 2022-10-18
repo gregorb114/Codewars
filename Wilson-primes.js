29e0380e2daf18d33762b233e72135fa50218191
gregor <gregor.boznar@gmail.com>
2022-10-18 10:46:38 +0200

    Add files via upload

---
+++ Wilson-primes.js
@@ -0,0 +1,9 @@
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+
+// Then,
+
+// ((P-1)! + 1) / (P * P)
+function amIWilson(p) {
+  if (n == 0) return 1;
+  else return n * factorial(n - 1);
+}
