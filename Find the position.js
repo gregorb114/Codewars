f6cc5fe7a3ba8549d17f567af5e0ee4e3f8b1459
gregor <gregor.boznar@gmail.com>
2022-10-28 10:00:00 +0200

    Add files via upload

---
+++ Find the position.js
@@ -0,0 +1,6 @@
+// When provided with a letter, return its position in the alphabet.
+
+function position(letter) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz";
+  return "Position of alphabet: " + (alphabet.indexOf(letter) + 1);
+}
