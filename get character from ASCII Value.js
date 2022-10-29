557e29104eee9bff3b582d497fb91a3a5f6c62db
gregor <gregor.boznar@gmail.com>
2022-10-29 23:52:38 +0200

    Add files via upload

--- get character from ASCII Value.js
+++ get character from ASCII Value.js
@@ -1,5 +1,4 @@
-//Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
-
-function getChar(c){
-    return String.fromCharCode(c)
-  }
+// Write a function get_char() / getChar() which takes a number and returns the corresponding ASCII char for that value.
+function getChar(c) {
+  return String.fromCharCode(c);
+}
