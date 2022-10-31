dec953e68c017dfe2c385ecbc6e71ef23fc6b833
gregor <gregor.boznar@gmail.com>
2022-10-31 20:29:03 +0100

    Add files via upload

---
+++ Add Length.js
@@ -0,0 +1,4 @@
+// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
+function addLength(str) {
+  return str.split(" ").map((s) => `${s} ${s.length}`);
+}
